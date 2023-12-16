1d0e71861b78cc0beddb3d2eaea894d7ab0dd39e
Ben Yomtov <benyomtov@comcast.net>
2023-12-16 11:58:32 -0500

    set up functionality for decrypter

--- enigma_frontend/src/App.js
+++ enigma_frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Encrypter from './components/Encrypter';
+import Decrypter from './components/Decrypter';
 
 
 function App() {
@@ -10,23 +11,8 @@ function App() {
     <div className="App">
       <h1>The Enigma Machine</h1>
       <Encrypter />
-
-        <div>
-        <h2>Decrypt a message</h2>
-        <form>
-          <p>Enter your key here:</p>
-          <input type="text" id="decryptkey" name="decryptkey" />
-          <p>Enter your encrypted message here:</p>
-          <textarea
-            name="messageBeforeDecryption"
-            id="messageBeforeDecryption"
-            rows="4"
-            cols="50"
-          ></textarea>
-          <br />
-          <input type="submit" value="Decrypt your message" />
-        </form>
-      </div>
+      <Decrypter />
+        
     </div>
   );
 }
